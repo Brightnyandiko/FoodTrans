1671658cab11bc34e24559f9a96b142e197048d2
Brightnyandiko <carlsonbright98@gmail.com>
2025-07-10 00:18:02 +0300

    updates

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,13 +8,14 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        margin: EdgeInsets.only(left: 20, top: 50),
-        child: Row(
+        margin: EdgeInsets.only(left: 18, top: 50),
+        child: Column(
           children: [
             Text(
-              "Login to your\naccount", 
-              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+              "Login to your\naccount",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30, color: Colors.black),
             ),
+            const SizedBox(height: 10,),
             Text("Please Sign in to your account")
           ],
         ),
